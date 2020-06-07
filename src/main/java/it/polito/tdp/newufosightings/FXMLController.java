5e6a9d75f5412b32bd854a72eb38e208558a3d6e
s244926 <s244926@studenti.polito.it>
2020-06-08 00:55:54 +0200

    Progetto Base turno A

---
+++ src/main/java/it/polito/tdp/newufosightings/FXMLController.java
@@ -0,0 +1,81 @@
+package it.polito.tdp.newufosightings;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.newufosightings.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+//controller turno A --> switchare al branch master_turnoB per turno B
+
+public class FXMLController {
+	
+	private Model model;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    private TextField txtAnno;
+
+    @FXML
+    private Button btnSelezionaAnno;
+
+    @FXML
+    private ComboBox<?> cmbBoxForma;
+
+    @FXML
+    private Button btnCreaGrafo;
+
+    @FXML
+    private TextField txtT1;
+
+    @FXML
+    private TextField txtAlfa;
+
+    @FXML
+    private Button btnSimula;
+
+    @FXML
+    void doCreaGrafo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doSelezionaAnno(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doSimula(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'NewUfoSightings.fxml'.";
+        assert txtAnno != null : "fx:id=\"txtAnno\" was not injected: check your FXML file 'NewUfoSightings.fxml'.";
+        assert btnSelezionaAnno != null : "fx:id=\"btnSelezionaAnno\" was not injected: check your FXML file 'NewUfoSightings.fxml'.";
+        assert cmbBoxForma != null : "fx:id=\"cmbBoxForma\" was not injected: check your FXML file 'NewUfoSightings.fxml'.";
+        assert btnCreaGrafo != null : "fx:id=\"btnCreaGrafo\" was not injected: check your FXML file 'NewUfoSightings.fxml'.";
+        assert txtT1 != null : "fx:id=\"txtT1\" was not injected: check your FXML file 'NewUfoSightings.fxml'.";
+        assert txtAlfa != null : "fx:id=\"txtAlfa\" was not injected: check your FXML file 'NewUfoSightings.fxml'.";
+        assert btnSimula != null : "fx:id=\"btnSimula\" was not injected: check your FXML file 'NewUfoSightings.fxml'.";
+
+    }
+
+	public void setModel(Model model) {
+		this.model = model;
+	}
+}
---
+++ src/main/java/it/polito/tdp/newufosightings/Main.java
@@ -0,0 +1,7 @@
+package it.polito.tdp.newufosightings;
+
+public class Main {
+	public static void main(String[] args) {
+        EntryPoint.main(args);
+    }
+}
